0ec5643b22c59bf9e7fc76e09eb5bb96793c5c52
jamesvan <jennyma228@hotmail.com>
2015-10-19 19:45:09 +0800

    modify html.sh

--- english/html.sh
+++ english/html.sh
@@ -3,7 +3,7 @@ gen_index()
 {
   rename 's/ /_/g' *;
   echo "" > index.html
-  echo "<html><head><title>www.cleverdog.cn</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\"/><script type=\"text/javascript\" src=\"/dog_files/player.js\"></script><script type=\"text/javascript\" src=\"/dog_files/jquery.js\"></script></head><body>" >> index.html
+  echo "<html><head><title>www.cleverdog.cn</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\"/><script type=\"text/javascript\" src=\"/dog_files/jquery.js\"></script><script type=\"text/javascript\" src=\"/dog_files/player.js\"></script></head><body>" >> index.html
 
   for f in `ls -F |grep "/$"`; do
     echo "<a href=\"${f}index.html\"/>${f}</a><br>" >> index.html
@@ -17,16 +17,19 @@ gen_index()
   for f in `ls *.gif`; do
     echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
   done
+
+  echo "<form id=\"form1\" name=\"form1\"><select name=\"mediafile\" id=\"mediafile\">" >> index.html
   for f in `ls *.mp4`; do
-    echo "<a href=\"javascript:playvideo('$f')\">$f</a><br>" >> index.html
+    echo "<option value=\"$f\">$f</option>" >> index.html
   done
+  echo "</select><input type=\"submit\" value=\"播放\"/><span id=\"msg\"></span></form><div class=\"myplayer\"></div>" >> index.html
+
   for f in `ls *.mp3`; do
     echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
   done
   for f in `ls *.MP3`; do
     echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
   done
-  echo "<div class=\"myplayer\"></div>" >> index.html
   echo "</body></html>" >> index.html
   chmod 755 index.html
 }
