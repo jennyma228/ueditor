fe3e48a258cbf7bc028e80c457ad81e2e3de3da7
jamesvan <jennyma228@hotmail.com>
2015-10-22 21:53:00 +0800

    refine shell

--- english/html.sh
+++ english/html.sh
@@ -20,7 +20,7 @@ gen_index()
   if [ "$files" != "0" ] ;then
     echo "<form id=\"form1\" name=\"form1\"><select name=\"mediafile\" id=\"mediafile\">" >> index.html
     for f in `find . -maxdepth 1 \( -type f -a -name "*.mp3" -o -name "*.mp4" -o -name "*.MP3" -o -name "*.MP4" \)`; do
-      echo "<option value=\"$f\">$f</option>" >> index.html
+      echo "<option value=\"${f:2}\">${f:2}</option>" >> index.html
     done
     echo "</select><input type=\"submit\" value=\"播放\"/><span id=\"msg\"></span></form><div class=\"myplayer\"></div>" >> index.html
   fi
