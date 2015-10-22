d4476bbc2246db99468057fa83b556b4c7098274
jamesvan <jennyma228@hotmail.com>
2015-10-22 21:08:10 +0800

    refine html.sh

--- english/html.sh
+++ english/html.sh
@@ -9,47 +9,20 @@ gen_index()
     echo "<a href=\"${f}index.html\"/>${f}</a><br>" >> index.html
   done
 
-  files=$(ls *.jpg 2> /dev/null | wc -l)
+  files=$(find . -maxdepth 1 \( -type f -a -name "*.bmp" -o -name "*.gif" -o -name "*.jpg" \) 2> /dev/null | wc -l)
   if [ "$files" != "0" ] ;then
-    for f in `ls *.jpg`; do
+    for f in `find . -maxdepth 1 \( -type f -a -name "*.bmp" -o -name "*.gif" -o -name "*.jpg" \)`; do
       echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
     done
   fi
 
-  files=$(ls *.bmp 2> /dev/null | wc -l)
-  if [ "$files" != "0" ] ;then
-    for f in `ls *.bmp`; do
-      echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
-    done
-  fi
-
-  if [ -e *.gif ]; then
-    for f in `ls *.gif`; do
-      echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
-    done
-  fi
-
-  files=$(ls *.mp4 2> /dev/null | wc -l)
+  files=$(find . -maxdepth 1 \( -type f -a -name "*.mp3" -o -name "*.mp4" -o -name "*.MP3" -o -name "*.MP4" \) 2> /dev/null | wc -l)
   if [ "$files" != "0" ] ;then
     echo "<form id=\"form1\" name=\"form1\"><select name=\"mediafile\" id=\"mediafile\">" >> index.html
-    for f in `ls *.mp4`; do
+    for f in `find . -maxdepth 1 \( -type f -a -name "*.mp3" -o -name "*.mp4" -o -name "*.MP3" -o -name "*.MP4" \)`; do
       echo "<option value=\"$f\">$f</option>" >> index.html
     done
     echo "</select><input type=\"submit\" value=\"播放\"/><span id=\"msg\"></span></form><div class=\"myplayer\"></div>" >> index.html
-  fi
-
-  files=$(ls *.mp3 2> /dev/null | wc -l)
-  if [ "$files" != "0" ] ;then
-    for f in `ls *.mp3`; do
-      echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
-    done
-  fi
-
-  files=$(ls *.MP3 2> /dev/null | wc -l)
-  if [ "$files" != "0" ] ;then
-    for f in `ls *.MP3`; do
-      echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
-    done
   fi
 
   echo "</body></html>" >> index.html
