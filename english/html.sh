612355390f9d9edf23dd0aa1639573f1cfa7d5ec
jamesvan <jennyma228@hotmail.com>
2015-10-22 17:23:05 +0800

    refine shell

--- english/html.sh
+++ english/html.sh
@@ -8,47 +8,69 @@ gen_index()
   for f in `ls -F |grep "/$"`; do
     echo "<a href=\"${f}index.html\"/>${f}</a><br>" >> index.html
   done
-  for f in `ls *.jpg`; do
-    echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
-  done
-  for f in `ls *.bmp`; do
-    echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
-  done
-  for f in `ls *.gif`; do
-    echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
-  done
 
-  echo "<form id=\"form1\" name=\"form1\"><select name=\"mediafile\" id=\"mediafile\">" >> index.html
-  for f in `ls *.mp4`; do
-    echo "<option value=\"$f\">$f</option>" >> index.html
-  done
-  echo "</select><input type=\"submit\" value=\"播放\"/><span id=\"msg\"></span></form><div class=\"myplayer\"></div>" >> index.html
+  files=$(ls *.jpg 2> /dev/null | wc -l)
+  if [ "$files" != "0" ] ;then
+    for f in `ls *.jpg`; do
+      echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
+    done
+  fi
 
-  for f in `ls *.mp3`; do
-    echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
-  done
-  for f in `ls *.MP3`; do
-    echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
-  done
+  files=$(ls *.bmp 2> /dev/null | wc -l)
+  if [ "$files" != "0" ] ;then
+    for f in `ls *.bmp`; do
+      echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
+    done
+  fi
+
+  if [ -e *.gif ]; then
+    for f in `ls *.gif`; do
+      echo "<img src=\"$f\" style=\"max-width: 98%; width: 223px; height: 216px;\" alt=\"$f\"/><br>" >> index.html
+    done
+  fi
+
+  files=$(ls *.mp4 2> /dev/null | wc -l)
+  if [ "$files" != "0" ] ;then
+    echo "<form id=\"form1\" name=\"form1\"><select name=\"mediafile\" id=\"mediafile\">" >> index.html
+    for f in `ls *.mp4`; do
+      echo "<option value=\"$f\">$f</option>" >> index.html
+    done
+    echo "</select><input type=\"submit\" value=\"播放\"/><span id=\"msg\"></span></form><div class=\"myplayer\"></div>" >> index.html
+  fi
+
+  files=$(ls *.mp3 2> /dev/null | wc -l)
+  if [ "$files" != "0" ] ;then
+    for f in `ls *.mp3`; do
+      echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
+    done
+  fi
+
+  files=$(ls *.MP3 2> /dev/null | wc -l)
+  if [ "$files" != "0" ] ;then
+    for f in `ls *.MP3`; do
+      echo "<a href=\"javascript:playaudio('$f')\">$f</a><br>" >> index.html
+    done
+  fi
+
   echo "</body></html>" >> index.html
   chmod 755 index.html
 }
 
 myfunc()
 {
-        for x in $(ls)
-        do
-                if [ -f "$x" ];then
-                        echo "$x";
-                elif [ -L "$x" ];then
-                        echo "this is a link";
-                elif [ -d "$x" ];then
-                        cd "$x";
-                        myfunc;
-                        gen_index;
-                        cd ..
-                fi
-         done
+  for x in $(ls)
+  do
+    if [ -f "$x" ];then
+      echo "$x";
+    elif [ -L "$x" ];then
+      echo "this is a link";
+    elif [ -d "$x" ];then
+      cd "$x";
+      myfunc;
+      gen_index;
+      cd ..
+    fi
+   done
 }
 
 myfunc
